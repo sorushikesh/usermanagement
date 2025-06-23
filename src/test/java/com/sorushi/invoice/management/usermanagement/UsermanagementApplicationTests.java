8c201a83ded42ea94854b6c253a40557f4485ee3
Rushikesh <sorushikesh07@gmail.com>
2025-06-24 01:08:47 +0530

    Initial commit

---
+++ src/test/java/com/sorushi/invoice/management/usermanagement/UsermanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sorushi.invoice.management.usermanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsermanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
